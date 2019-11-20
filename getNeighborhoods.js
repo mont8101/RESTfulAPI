7235415be33ea347b9d374c5387eb5b325a3f57c
mont8101 <55461476+mont8101@users.noreply.github.com>
2019-11-19 19:49:40 -0600

    Update getNeighborhoods.js

--- getNeighborhoods.js
+++ getNeighborhoods.js
@@ -13,23 +13,56 @@ var db_filename = path.join(__dirname, 'stpaul_crime.sqlite3');
 var app = express();
 var port = 8000;
 
-// open usenergy.sqlite3 database
+// open stpaul_crime.sqlite3 database
 var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     if (err) {
         console.log('Error opening ' + db_filename);
     }
     else {
-        console.log('Now connected to ' + db_filename + '\n');
+        console.log('Now reading ' + db_filename + '\n');
     }
 });
 
 app.use(express.static(public_dir));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/codes", (req, res)=>{
+    db.all("SELECT * FROM codes", (err, rows)=>{
+        var dbCodes = "";
+        for(i = 0; i < rows.length; i++) {
+            dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(dbCodes);
+    });
+});
+
 app.get("/neighborhoods", (req, res)=>{
     db.all("SELECT * FROM neighborhoods", (err, rows)=>{
-        console.log(rows);
+        var dbNeighborhoods = "";
+        for(i = 0; i < rows.length; i++) {
+            dbNeighborhoods = dbNeighborhoods + '"N' + rows[i]["neighborhood_number"] + '": "' + rows[i]["neighborhood_name"] + '",' + "\n"
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(dbNeighborhoods);
     });
-    
+});
+
+app.get("/incidents", (req, res)=>{
+    db.all("SELECT * FROM incidents", (err, rows)=>{
+        var dbIncidents = "";
+        for(i = 0; i < rows.length; i++) {
+            dbIncidents = dbIncidents + '"I' + rows[i]["case_number"] + '": {' + "\n" +
+            '"time": "' + rows[i]["date_time"] + '",' + "\n" +
+            '"code": ' + rows[i]["code"] + ',' + "\n" +
+            '"incident": "' + rows[i]["incident"] + '",' + "\n" +
+            '"police_grid": ' + rows[i]["police_grid"] + ',' + "\n" +
+            '"neighborhood_number": ' + rows[i]["neighborhood_number"] + ',' + "\n" +
+            '"block": "' + rows[i]["block"] + '",' + "\n" + "}," +"\n"       
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(dbIncidents);
+    });
 });
 console.log('Now listening on port ' + port);
 app.listen(port);
