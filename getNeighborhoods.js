81812af2cfbbd6fd012e4dfd4ebc59a548d848c7
scottdobber <55461492+scottdobber@users.noreply.github.com>
2019-11-19 22:22:32 -0600

    fixed Codes
    AAAAAAA

--- getNeighborhoods.js
+++ getNeighborhoods.js
@@ -26,15 +26,69 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 app.use(express.static(public_dir));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/codes", (req, res)=>{
+app.get("/codes", (req, res)=>{        
+    var code = req.query.code;
+    /*if(code == null){
+        db.all("SELECT * FROM codes", (err, rows)=>{
+            //console.log(code);
+            var dbCodes = "";
+            //console.log(rows[4]);
+            for(i = 0; i < rows.length; i++) {
+                dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).send(dbCodes);
+            
+            db.close();
+        });       
+    }else{
+        db.all("SELECT * FROM codes", (err, rows)=>{
+            var dbCodes = "";
+            code = code.toString();
+            var codeArr = code.split(",");
+            console.log(rows.length);
+            console.log(codeArr);
+            for(var i = 0; i<rows.length; i++){
+                for(var j = 0; j<codeArr.length; j++){
+                    //console.log(i)
+                    if (rows[i]["code"]==codeArr[j]){
+                        dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+                    }
+                }
+                
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).send(dbCodes);
+            console.log(dbCodes);
+            db.close();
+        });  
+    }*/
     db.all("SELECT * FROM codes", (err, rows)=>{
         var dbCodes = "";
-        for(i = 0; i < rows.length; i++) {
-            dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+
+        if(code == null){
+            for(i = 0; i < rows.length; i++) {
+                dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+            }
+        }else{
+            code = code.toString();
+            var codeArr = code.split(",");
+            console.log(rows.length);
+            console.log(codeArr);
+            for(var i = 0; i<rows.length; i++){
+                for(var j = 0; j<codeArr.length; j++){
+                    //console.log(i)
+                    if (rows[i]["code"]==codeArr[j]){
+                        dbCodes = dbCodes + '"C' + rows[i]["code"] + '": "' + rows[i]["incident_type"] + '",' + "\n"
+                    }
+                }
+                
+            }
         }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).send(dbCodes);
-    });
+    });  
+    
 });
 
 app.get("/neighborhoods", (req, res)=>{
